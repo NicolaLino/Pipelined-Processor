60d2a1d3ec582445285b36ff8043cee01c86bf63
NicolaLino <nicola.abu.shaibeh@gmail.com>
2023-09-09 19:33:52 +0300

    fix some errors

--- ExtendByte.sv
+++ ExtendByte.sv
@@ -19,14 +19,14 @@
 
 module ExtendByte(
     output wire [31:0] extendedValue,
-    input wire [7:0] byte,
+    input wire [7:0] byteValue,
     input wire SIG_ExtByte, // 1 for sign extension, 0 for zero extension
 );
     wire [31:0] signExtended;
     wire [31:0] zeroExtended;
 
-    assign signExtended = {{24{byte[7]}}, byte};
-    assign zeroExtended = {24'b0, byte};
+    assign signExtended = {{24{byteValue[7]}}, byteValue};
+    assign zeroExtended = {24'b0, byteValue};
 
     assign extendedValue = SIG_ExtByte ? signExtended : zeroExtended;
 endmodule
